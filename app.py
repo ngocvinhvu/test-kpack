6a53cbb3753baada67d342898f4e638c0fa3047e
vinhvn <vungocvinh95@gmail.com>
2023-08-31 09:41:42 +0700

    init

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Vinhvn4'
+    return 'Hello, ho so li li'
 
 @app.route('/hello/<username>')
 def hello_user(username):
